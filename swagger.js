412ec7bf13c042561ed4bd1dc897ef8cc7c9e6a8
l-gomes-ar <lgaraujo021@gmail.com>
2024-05-12 04:35:49 +0100

    Updated description and host on swagger api doc

--- swagger.js
+++ swagger.js
@@ -3,9 +3,9 @@ const swaggerAutogen = require("swagger-autogen");
 const doc = {
     info: {
         title: "Library API",
-        description: "CSE 341 Project 2 - Library API"
+        description: "CSE 341 Project 2 - Library API by Lucas Araujo"
     },
-    host: "localhost:6060"
+    host: "cse-341-project2-21jp.onrender.com"
 };
 
 const outputFile = "./swagger-output.json";
